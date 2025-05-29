46fafe5a3635744dc08cca8e70bb56008e9baff6
IntegraseOnline <36938470+SamuelMBote@users.noreply.github.com>
2025-05-29 14:25:12 -0300

    Not Found

---
+++ src/app/global-error.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+const GlobalError = () => {
+  return (
+    <html>
+      <body>
+        <h1>Um erro ocorreu</h1>
+      </body>
+    </html>
+  );
+};
+
+export default GlobalError;
